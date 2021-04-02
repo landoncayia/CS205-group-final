938ce1ca226a7f35f124005bc162350f76895e48
echonorcott <enorcott@uvm.edu>
2021-04-02 15:39:23 -0400

    got it printing test piece badly & broke add_piece

--- driver.py
+++ driver.py
@@ -3,26 +3,19 @@ import pygame.locals
 from board import Board
 from piece import Piece
 from piece import Shape
+from piece import create_set
 from tile import Color
 from tile import Tile
 
 BOARD_WIDTH, BOARD_HEIGHT = 800, 800
 
 
-def create_set(set_color):
-    i = 1
-    piece_list = []
-    tile_x_start = 900
-    tile_y_start = 50
-    while (i < 22):
-        piece_list.append(Piece(Shape(i), Tile(tile_x_start, tile_y_start, set_color)))
-    return piece_list
-
 if __name__ == '__main__':
     pygame.init()
     window = (1200, 800)
     screen = pygame.display.set_mode(window)
     screen.fill(Color.BG_GREY.value)
+    piecesSurface = pygame.Surface((200, 200))
 
     # Create and draw a board, then put it on the screen
     board = Board(window)
@@ -34,8 +27,14 @@ if __name__ == '__main__':
                                       BOARD_HEIGHT//2-board.get_surface().get_height()//2))
 
     # DISPLAY PIECES
-    full_set = create_set(Color.BLUE)
-    i = 0
+    piecesSurface.fill(Color.BG_GREY.value)
+    testpiece.draw_piece(piecesSurface)
+    screen.blit(piecesSurface, (800, 200))
+    # full_set = create_set(Color.BLUE, 900, 50)
+
+    # testpiece.draw(piecesSurface)
+    # while (i < (len(full_set) / 2)):
+    #     full_set[i].draw(piecesSurface)
 
     pygame.display.flip()
     done = False
